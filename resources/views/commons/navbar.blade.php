24183466a486192793506e521e9548a504af21b7
taiseihorikawa <taisei_horikawa@yahoo.co.jp>
2020-09-08 19:55:11 +0000

    task-management

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -2,6 +2,10 @@
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         {{-- トップページへのリンク --}}
         <a class="navbar-brand" href="/">Task-management</a>
+        {{-- ユーザ登録ページへのリンク --}}
+         <h1>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</h1>
+          {{-- ログインページへのリンク --}}
+                    <h1 class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</h1>
 
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
             <span class="navbar-toggler-icon"></span>
@@ -12,6 +16,9 @@
             <ul class="navbar-nav">
                 {{-- メッセージ作成ページへのリンク --}}
                 <li class="nav-item">{!! link_to_route('tasks.create', '新規メッセージの投稿', [], ['class' => 'nav-link']) !!}</li>
+                 {{-- ログアウトへのリンク --}}
+                            <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
+                            
             </ul>
         </div>
     </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,6 +65,17 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
+            @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
+    @endif
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
